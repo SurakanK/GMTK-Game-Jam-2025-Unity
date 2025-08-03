358a0fd6e6dedf75dad77e188733e7987ed68ae3
IrisDeMort (Dragon) <nawatng@gmail.com>
2025-08-03 14:09:55 +0700

    Add sound on menu and town + Show tutorial on fist day

---
+++ Assets/Script/Town/TownEvenrManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TownEvenrManager : MonoBehaviour
+{
+    public static TownEvenrManager Instance { get; private set; }
+
+     private DialogBox Banker_box;
+     private DialogBox Shopkeeper_box;
+
+    [SerializeField] private GameObject Banker;
+    [SerializeField] private GameObject Shopkeeper;
+    [SerializeField] private GameObject Cave;
+
+    [Header("Dialog List")]
+    [SerializeField] DialogInfoSO[] BankerDialogData;
+    [SerializeField] DialogInfoSO[] ShopkeeperDialogData;
+
+
+    [SerializeField] bool IsFirstDay = true;
+    [SerializeField] GameObject Tutorial;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Ensure only one instance exists
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Optional: keep across scenes
+        }
+    }
+
+    private void Start()
+    {
+        Banker_box = Banker.GetComponent<DialogBox>();
+        Shopkeeper_box = Shopkeeper.GetComponent<DialogBox>();
+
+        Button caveButton = Cave.GetComponent<Button>();
+        if (IsFirstDay) caveButton.onClick.AddListener(OnCaveButtonClicked);
+    }
+
+    private void OnCaveButtonClicked()
+    {
+        if (Tutorial != null)
+        {
+            Tutorial.SetActive(true);
+        }
+
+        IsFirstDay = false;
+        Debug.Log("Tutorial activated, IsFirstDay set to false.");
+    }
+
+    public void UpdateBankNoteDialog()
+    {
+        Banker_box.SetNewDialog(BankerDialogData[1]);
+    }
+
+    public void ResetAllDialog()
+    {
+        Banker.SetActive(true);
+        Shopkeeper.SetActive(true);
+        Banker_box.SetNewDialog(BankerDialogData[0]);
+        Shopkeeper_box.SetNewDialog(ShopkeeperDialogData[0]);
+    }
+   
+}
